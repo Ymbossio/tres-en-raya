b9324c4db1e64f526ed57f5ed530039440601f72
Yovanis Bossio <129323785+Ymbossio@users.noreply.github.com>
2024-05-27 23:03:51 +0000

    Pintando el tablero

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
-import './App.css'
+import { useState } from "react"
+
+const TURNS = {
+  x: 'x',
+  O: 'o'
+}
+
+
+
+const Square = ({children, updateBoard, index}) =>{
+  return (
+      <div className="square" key={index}>
+        {children}
+    </div>
+  )
+}
 
 function App() {
+  const [board, setBoard] = useState( Array(9).fill(null));
+  const [turn, setTurn] = useState(TURN.X)
 
   return (
-    <>
-      <h1>Tic Tac Toe</h1>
-    </>
+      <main className="board">
+        <h1>Tic Tac Toe</h1>
+        <section className="game">
+          {board.map((_, index) =>{
+            return(
+              <Square key={index} index={index}/>
+            )
+          })}
+        </section>
+
+        <section className="turn">
+          <Square isSelected={turn == TURNS.X}>{TURNS.X}</Square>
+          <Square isSelected={turn == TURNS.O}>{TURNS.O}</Square>
+        </section>
+      </main>
   )
 }
 
